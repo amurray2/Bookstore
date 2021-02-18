2725d9def63fdbede383090f50b03d7dd432e23d
amurray2 <42621202+amurray2@users.noreply.github.com>
2021-02-17 17:27:05 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        //This is my model
+        [Key]
+        public int BookId { get; set; } //This is my Private key
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthFirstName { get; set; } //I split up the first, middle, and last name
+        public string AuthMiddleName { get; set; }
+        [Required]
+        public string AuthLastName { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        [RegularExpression(@"^([0-9]{3})-([0-9]{10})$")]
+        public string ISBN { get; set; } //accepts the following format '###-##########'
+        [Required]
+        public bool Fiction { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder application)
+        {
+            BookDbContext context = application.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BookDbContext>();
+
+            if(context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Books.Any())
+            {
+                context.Books.AddRange(
+                    new Book
+                    {
+                        Title = "Les Miserables",
+                        AuthFirstName = "Victor",
+                        AuthLastName = "Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439",
+                        Fiction = true,
+                        Category = "Classic",
+                        Price = 9.95m
+                    },
+
+                    new Book
+                    {
+                        Title = "Team of Rivals",
+                        AuthFirstName = "Doris",
+                        AuthMiddleName = "Kearns",
+                        AuthLastName = "Goodwin",
+                        Publisher = "Simon & Schuster",
+                        ISBN = "978-0743270755",
+                        Fiction = false,
+                        Category = "Biography",
+                        Price = 14.58m
+                    },
+
+                    new Book
+                    {
+                        Title = "The Snowball",
+                        AuthFirstName = "Alice",
+                        AuthLastName = "Schroeder",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553384611",
+                        Fiction = false,
+                        Category = "Biography",
+                        Price = 21.54m
+                    },
+
+                    new Book
+                    {
+                        Title = "American Ulysses",
+                        AuthFirstName = "Ronald",
+                        AuthMiddleName = "C.",
+                        AuthLastName = "White",
+                        Publisher = "Random House",
+                        ISBN = "978-0812981254",
+                        Fiction = false,
+                        Category = "Biography",
+                        Price = 11.61m
+                    },
+
+                    new Book
+                    {
+                        Title = "Unbroken",
+                        AuthFirstName = "Laura",
+                        AuthLastName = "Hillenbrand",
+                        Publisher = "Random House",
+                        ISBN = "978-0812974492",
+                        Fiction = false,
+                        Category = "Historical",
+                        Price = 13.33m
+                    },
+
+                    new Book
+                    {
+                        Title = "The Great Train Robbery",
+                        AuthFirstName = "Michael",
+                        AuthLastName = "Crichton",
+                        Publisher = "Vintage",
+                        ISBN = "978-0804171281",
+                        Fiction = true,
+                        Category = "Historical Fiction",
+                        Price = 15.95m
+                    },
+
+                    new Book
+                    {
+                        Title = "Deep Work",
+                        AuthFirstName = "Cal",
+                        AuthLastName = "Newport",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455586691",
+                        Fiction = false,
+                        Category = "Self-Help",
+                        Price = 14.99m
+                    },
+
+                    new Book
+                    {
+                        Title = "It's Your Ship",
+                        AuthFirstName = "Michael",
+                        AuthLastName = "Abrashoff",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455523023",
+                        Fiction = false,
+                        Category = "Self-Help",
+                        Price = 21.66m
+                    },
+
+                    new Book
+                    {
+                        Title = "The Virgin Way",
+                        AuthFirstName = "Richard",
+                        AuthLastName = "Branson",
+                        Publisher = "Portfolio",
+                        ISBN = "978-1591847984",
+                        Fiction = false,
+                        Category = "Business",
+                        Price = 29.16m
+                    },
+
+                    new Book
+                    {
+                        Title = "Sycamore Row",
+                        AuthFirstName = "John",
+                        AuthLastName = "Grisham",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553393613",
+                        Fiction = true,
+                        Category = "Thrillers",
+                        Price = 15.03m
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
